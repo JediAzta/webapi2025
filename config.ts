3b0a07f27314f47e2e63f871987d05a743dbe7e6
Master Chit Chit <aztawong@aliyun.com>
2025-03-05 21:55:00 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 export const config = {
     host: 'localhost',
     port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'blogdb',
+    user: '',
+    password: '',
+    database: '',
     connection_limit: 100
 }
